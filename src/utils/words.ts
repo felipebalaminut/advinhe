3f5d0c80e46d6ac26b229bcafdf9766fd5802ce0
Felipe Fonseca <felipebalaminutdev@gmail.com>
2025-09-08 13:53:52 -0300

    initial commit

---
+++ src/utils/words.ts
@@ -0,0 +1,10 @@
+export type Challenge = {
+  id: number;
+  word: string;
+  tip: string;
+};
+
+export const WORDS: Challenge[] = [
+  { id: 1, word: "CSS", tip: "Linguagem de estilos" },
+  { id: 2, word: "REACT", tip: "Biblioteca para criar interfaces Web" },
+];
